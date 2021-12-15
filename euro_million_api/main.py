0c0d2e7e552d976fbc256c0ff0ec314a686620f8
Ichrak CHEKKAR <chekkarich@cy-tech.fr>
2021-12-15 23:00:06 +0100

    main.py again

---
+++ euro_million_api/main.py
@@ -0,0 +1,46 @@
+import joblib #to save the model (not like pickel)
+import re 
+from fastapi import FastAPI 
+from pydantic import BaseModel
+from typing import Optional 
+
+class combinaison(BaseModel):
+    combi_id : str
+    description : Optional[str] = None
+    N1 : int 
+    N2 : int 
+    N3 : int
+    N4 : int
+    N5 : int
+    E1 : int
+    E2 : int
+    probToWin : Optional[int] = None 
+    
+app = FastAPI()
+
+@app.get('/')
+
+def root():
+    return {"message" : "Prediction Tirage Euro Million! We gonna be rich my friend"}
+
+@app.get("/items/{item_id}")
+async def read_combi(i)
+
+@app.post('/api/predict')
+
+def get_prediction():
+    #Donne la proba pour que la combi d'entrée soit gagnante
+    return "proba gain + proba perte"
+
+
+
+@app.get('/api/predict')
+
+def generate_combi():
+    # Genere une combi gagnante ( avec une proba gain élevée)
+    # à définir c'est quoi élevée
+    return "This combi is good"
+
+
+#@app.get('api/model') 
+
